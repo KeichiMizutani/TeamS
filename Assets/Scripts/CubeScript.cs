7bf30567b140d2584899afc47ec1edf6c4389251
MittaniK1 <m.keichi16256@gmail.com>
2020-09-07 01:37:46 +0900

    試製六角形ステージ＆試製キャラUI

--- Assets/Scripts/CubeScript.cs
+++ Assets/Scripts/CubeScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CubeScript : MonoBehaviour
 {
-    public Color32[] colors = new Color32[4];
+    public Color32[] colors = new Color32[3];
 
     [SerializeField]
     private Renderer targetRenderer;
@@ -15,13 +15,26 @@ public class CubeScript : MonoBehaviour
     {
         colors[0] = new Color32(255, 100, 100, 255);
         colors[1] = new Color32(255, 255, 100, 255);
-        colors[2] = new Color32(100, 255, 100, 255);
-        colors[3] = new Color32(100, 100, 255, 255);
+        colors[2] = new Color32(100, 100, 255, 255);
 
-        colorNum = Random.Range(0, 4);
+        colorNum = Random.Range(0, 3);
 
         targetRenderer = this.GetComponent<Renderer>();
         targetRenderer.material.SetColor("_BaseColor", colors[colorNum]);
 
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("z"))
+        {
+            colorNum++;
+            if(colorNum > 2)
+            {
+                colorNum = 0;
+            }
+            targetRenderer.material.SetColor("_BaseColor", colors[colorNum]);
+        }
     }
 }
--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -9,13 +9,22 @@ public class StageManager : MonoBehaviour
 
     public int length;
     public int width;
+    public float distance;
+
     void Start()
     {
-        for(int i = 0; i < width; i++)
+        for(int dz = 0; dz < length; dz++)
         {
-            for (int j = 0; j < length; j++)
+            for (int dx = 0; dx < width; dx++)
             {
-                Instantiate(plane, new Vector3(i, 0, j), transform.rotation);
+                //Instantiate(plane, new Vector3(i * distance, 0, j), transform.rotation);
+                if(dz % 2 == 0)
+                {
+                    Instantiate(plane, new Vector3(dx * distance, 0, dz * distance * Mathf.Sin(Mathf.PI / 3)), transform.rotation);
+                } else
+                {
+                    Instantiate(plane, new Vector3(dx * distance + distance / 2, 0, dz * distance * Mathf.Sin(Mathf.PI / 3)), transform.rotation);
+                }
             }
         }
     }
