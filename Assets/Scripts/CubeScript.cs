b77c42136bf67fd8e158fc3403226f17b3ba663e
MittaniK1 <m.keichi16256@gmail.com>
2020-09-06 12:41:11 +0900

    TeamS　初回床作成　試製

---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    public Color32[] colors = new Color32[4];
+
+    [SerializeField]
+    private Renderer targetRenderer;
+
+    int colorNum;
+
+    private void Start()
+    {
+        colors[0] = new Color32(255, 100, 100, 255);
+        colors[1] = new Color32(255, 255, 100, 255);
+        colors[2] = new Color32(100, 255, 100, 255);
+        colors[3] = new Color32(100, 100, 255, 255);
+
+        colorNum = Random.Range(0, 4);
+
+        targetRenderer = this.GetComponent<Renderer>();
+        targetRenderer.material.SetColor("_BaseColor", colors[colorNum]);
+
+    }
+}
---
+++ Assets/Scripts/StageManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject plane;
+
+    public int length;
+    public int width;
+    void Start()
+    {
+        for(int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < length; j++)
+            {
+                Instantiate(plane, new Vector3(i, 0, j), transform.rotation);
+            }
+        }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
