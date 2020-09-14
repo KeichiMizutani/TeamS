c663d3372144fff9ad3ac0cbcf80e828a34d8b4a
MittaniK1 <m.keichi16256@gmail.com>
2020-09-14 20:59:05 +0900

    Lerp Test

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,17 +4,54 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    Animator animator;
+
+    [SerializeField]
+    private GameObject moveStart;
+    [SerializeField]
+    private GameObject moveGoal;
+
+    private bool isMoving = false;
+    public float moveSpeed = 1.0f;
+    private float moveTimer = 0;
+
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        animator.SetBool("Run", true);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("Jump", true);
+            isMoving = true;
+        }
+
+        if (!isMoving) {
+            moveStart.transform.position = transform.position;
+            moveGoal.transform.position = transform.position + transform.forward * 1.75f;
+        }
+        else
+        {
+            moveTimer += Time.deltaTime;
+            float location = moveTimer * moveSpeed / 1.75f;
+            transform.position = Vector3.Lerp(moveStart.transform.position, moveGoal.transform.position, location);
+
+            if (transform.position == moveGoal.transform.position)
+            {
+                animator.SetBool("Jump", false);
+                isMoving = false;
+                moveTimer = 0f;
+            }
+        }
+
+
     }
+
 
     public void OnCallChangeFace()
     {
